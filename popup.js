d0a025af65d25db14a7aae314f47936783d6559b
Dilraj Singh <dilrajsingh.7@gmail.com>
2021-02-28 23:23:24 -0800

    Wrap code in render() function

--- popup.js
+++ popup.js
@@ -6,74 +6,80 @@
  */
 
 (function () {
-  chrome.windows.getAll({ populate: true }, (windows) => {
-    const outerDiv = document.createElement("div");
+  function render() {
+    chrome.windows.getAll({ populate: true }, (windows) => {
+      const outerDiv = document.createElement("div");
 
-    const warning = document.createElement("div");
-    warning.className = "warning";
-    warning.innerText = "Do NOT freeze tabs with unsaved work!";
+      const warning = document.createElement("div");
+      warning.className = "warning";
+      warning.innerText = "Do NOT freeze tabs with unsaved work!";
 
-    const discardAllWindows = document.createElement("button");
-    discardAllWindows.textContent = "Freeze all Tabs in all Windows";
-    discardAllWindows.onclick = () => {
-      windows.forEach((w) => w.tabs.forEach((t) => chrome.tabs.discard(t.id)));
-    };
-
-    outerDiv.appendChild(warning);
-    outerDiv.appendChild(discardAllWindows);
-
-    windows.forEach((w) => {
-      /**
-       * The Summary of the div
-       * e.g.
-       *
-       * Window 13 <Discard All>
-       */
-      const summary = document.createElement("summary");
-
-      const discardedTabs = w.tabs.filter((t) => t.discarded).length;
-
-      let titleDiv;
-      const titleText = `Window (${discardedTabs}/${w.tabs.length} frozen tabs)`;
-      if (w.focused) {
-        titleDiv = document.createElement("MARK");
-        titleDiv.innerText = titleText;
-      } else {
-        titleDiv = document.createTextNode(titleText);
-      }
-
-      const discardAllButton = document.createElement("BUTTON");
-      discardAllButton.textContent = "Freeze all tabs";
-      discardAllButton.onclick = () => {
-        w.tabs.forEach((t) =>
-          chrome.tabs.discard(t.id, (tabOrUndefined) => {})
+      const discardAllWindows = document.createElement("button");
+      discardAllWindows.textContent = "Freeze all Tabs in all Windows";
+      discardAllWindows.onclick = () => {
+        windows.forEach((w) =>
+          w.tabs.forEach((t) => chrome.tabs.discard(t.id))
         );
       };
 
-      summary.appendChild(titleDiv);
-      summary.appendChild(discardAllButton);
+      outerDiv.appendChild(warning);
+      outerDiv.appendChild(discardAllWindows);
 
-      /**
-       * Tabs in each window
-       * e.g.
-       *    * Design the user interface - Chrome Developers
-       *    * HTML 5 <details> Tag
-       */
+      windows.forEach((w) => {
+        /**
+         * The Summary of the div
+         * e.g.
+         *
+         * Window 13 <Discard All>
+         */
+        const summary = document.createElement("summary");
 
-      const tabList = document.createElement("ul");
-      w.tabs.forEach((t) => {
-        const tabSpan = document.createElement("li");
-        tabSpan.textContent = `${t.title}`;
-        tabList.appendChild(tabSpan);
+        const discardedTabs = w.tabs.filter((t) => t.discarded).length;
+
+        let titleDiv;
+        const titleText = `Window (${discardedTabs}/${w.tabs.length} frozen tabs)`;
+        if (w.focused) {
+          titleDiv = document.createElement("MARK");
+          titleDiv.innerText = titleText;
+        } else {
+          titleDiv = document.createTextNode(titleText);
+        }
+
+        const discardAllButton = document.createElement("BUTTON");
+        discardAllButton.textContent = "Freeze all tabs";
+        discardAllButton.onclick = () => {
+          w.tabs.forEach((t) =>
+            chrome.tabs.discard(t.id, (tabOrUndefined) => {})
+          );
+        };
+
+        summary.appendChild(titleDiv);
+        summary.appendChild(discardAllButton);
+
+        /**
+         * Tabs in each window
+         * e.g.
+         *    * Design the user interface - Chrome Developers
+         *    * HTML 5 <details> Tag
+         */
+
+        const tabList = document.createElement("ul");
+        w.tabs.forEach((t) => {
+          const tabSpan = document.createElement("li");
+          tabSpan.textContent = `${t.title}`;
+          tabList.appendChild(tabSpan);
+        });
+
+        // done with window
+        const details = document.createElement("details");
+        details.appendChild(summary);
+        details.appendChild(tabList);
+        outerDiv.appendChild(details);
       });
 
-      // done with window
-      const details = document.createElement("details");
-      details.appendChild(summary);
-      details.appendChild(tabList);
-      outerDiv.appendChild(details);
+      document.getElementById("app").appendChild(outerDiv);
     });
+  }
 
-    document.getElementById("app").appendChild(outerDiv);
-  });
+  render();
 })();
